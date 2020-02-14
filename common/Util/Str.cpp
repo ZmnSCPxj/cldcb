b1b7f86c7b0ec1312d4a1bede26cc1b087845611
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-02-14 04:56:43 +0000

    common/Util/Str.cpp: Create function to parse hex buffers.

--- common/Util/Str.cpp
+++ common/Util/Str.cpp
@@ -17,5 +17,41 @@ std::string hexbyte(std::uint8_t v) {
 	return os.str();
 }
 
+namespace {
+
+std::uint8_t parse_hex(char c) {
+	if (('0' <= c) && (c <= '9')) {
+		return (std::uint8_t) (c & 0xF);
+	} else if ((('a' <= c) && (c <= 'f')) ||
+		   (('A' <= c) && (c <= 'F'))) {
+		return (std::uint8_t) ((c + 9) & 0xF);
+	} else {
+		char s[2];
+		s[0] = c;
+		s[1] = 0;
+		throw HexParseFailure("Non-hex character: " + std::string(s));
+	}
+}
+
+std::uint8_t parse_hex_byte(char c0, char c1) {
+	return (parse_hex(c0) << 4) | (parse_hex(c1));
+}
+
+}
+
+std::vector<std::uint8_t> hexread(std::string const& s) {
+	if ((s.length() % 2) != 0)
+		throw HexParseFailure("String length must be even.");
+
+	auto buflen = s.length() / 2;
+	auto buf = std::vector<std::uint8_t>(buflen);
+
+	for (auto i = 0; i < buflen; ++i) {
+		buf[i] = parse_hex_byte(s[i * 2], s[i * 2 + 1]);
+	}
+
+	return buf;
+}
+
 }
 }
--- common/Util/Str.hpp
+++ common/Util/Str.hpp
@@ -6,13 +6,23 @@
  */
 
 #include<cstdint>
+#include<stdexcept>
 #include<string>
+#include<vector>
 
 namespace Util {
 namespace Str {
 
 /* Outputs a two-digit hex string of the given byte.  */
 std::string hexbyte(std::uint8_t);
+
+/* Creates a buffer from the given hex string.  */
+/* FIXME: use a backtrace-extracting exception. */
+struct HexParseFailure : public std::runtime_error {
+	HexParseFailure(std::string msg)
+		: std::runtime_error("hexread: " + msg) { }
+};
+std::vector<std::uint8_t> hexread(std::string const&);
 
 }
 }
