3ae6c44e3804939cd72b5b2fa5ee6f0a96b8da70
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-02-20 16:48:26 +0800

    common/Sha256/fun.cpp: Use the libsodium function instead.
    Also remove the sha-2 library.

--- common/Sha256/fun.cpp
+++ common/Sha256/fun.cpp
@@ -1,4 +1,4 @@
-#include<sha-256.h>
+#include<sodium/crypto_hash_sha256.h>
 #include"Sha256/Hash.hpp"
 #include"Sha256/fun.hpp"
 
@@ -6,7 +6,10 @@ namespace Sha256 {
 
 Hash fun(void const *p, std::size_t len) {
 	std::uint8_t hash[32];
-	calc_sha_256(hash, p, len);
+	crypto_hash_sha256( hash
+			  , reinterpret_cast<unsigned char const*>(p)
+			  , len
+			  );
 	return Hash::from_buffer(hash);
 }
 
