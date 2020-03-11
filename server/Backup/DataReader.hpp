a8097dc889416c0e6202d1bcfa10fc346f2b95f2
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-03-11 09:04:45 +0000

    server/Backup/DataReader.hpp: Change how to report back.

--- server/Backup/DataReader.hpp
+++ server/Backup/DataReader.hpp
@@ -21,18 +21,26 @@ public:
 	Ev::Io<std::unique_ptr<std::vector<std::uint8_t>>>
 	backedup_reupload_chunk() =0;
 
+	enum IncrementType
+	{ New
+	, Chunk
+	, EndAll
+	};
+
+	struct IncrementMsg {
+		IncrementType type;
+		/* Filled in if type == New.  */
+		std::uint32_t data_version;
+		/* Filled in if type == Chunk.  */
+		std::vector<std::uint8_t> chunk;
+	};
+
 	/* Returns nullptr if an error occurred.
-	 * The paired uint32_t is the data_version.
-	 * Returns a vector of size 0 if ended *all* chunks
-	 * with all `data_version`s.
+	 * Else returns the result.
 	 */
 	virtual
-	Ev::Io<
-		std::unique_ptr<std::pair< std::uint32_t
-					 , std::vector<std::uint8_t>
-					 >>
-	      >
-	backedup_incremental_chunk() =0;
+	Ev::Io<std::unique_ptr<IncrementMsg>>
+	backedup_incremental_get() =0;
 };
 
 }
