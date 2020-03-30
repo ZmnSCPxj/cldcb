8f0dc462387f042179d8d1b0b21f47fc852a5b1d
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-03-30 16:53:44 +0800

    server/test_io_leak.cpp: Fix some infelicity of GCC.

--- server/test_io_leak.cpp
+++ server/test_io_leak.cpp
@@ -7,7 +7,17 @@ void* operator new(unsigned long size) {
 	++count;
 	return malloc(size);
 }
-void operator delete(void* p) throw() {
+void operator delete(void* p) noexcept {
+	--count;
+	free(p);
+}
+/* For some reason, GCC 9, on -O2, uses this function to
+ * delete an object it allocated with the above operator
+ * new, which massively confuses our count (and raises
+ * an error in valgrind).
+ * It is not used on GCC 9 on -00 though....
+ */
+void operator delete(void* p, unsigned long) noexcept {
 	--count;
 	free(p);
 }
